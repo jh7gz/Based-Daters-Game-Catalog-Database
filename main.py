a94bb9a384f58dba191ca2bce15d1ce097310c8d
jh7gz <jh7gz@umsystem.edu>
2026-05-02 00:14:51 -0500

    Implemented!!! Coding may begin.

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 #import functionalRequirements as fr
 #import aggregationFunctions as af
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root"
+)
+
+cursor = conn.cursor()
 
 def Initial_setup():
-    # Check if database "gameCatalogs" exists on this localhost
-    # If not, run setup.sql file
-    pass
+    print("Initializing setup...")
+    cursor.execute("""
+        SELECT SCHEMA_NAME 
+        FROM INFORMATION_SCHEMA.SCHEMATA 
+        WHERE SCHEMA_NAME = "gameCatalogs"
+    """)
+    result = cursor.fetchone()
+    if result:
+        print("Database gameCatalogs already exists!")
+    else:
+        print("Lets create Database gameCatalogs")
+
+        # Create and use
+        cursor.execute("CREATE DATABASE gameCatalogs")
+        cursor.execute("USE gameCatalogs")
+
+        # Read and execute setup.sql file
+        with open("setupNoComments.sql", "r", encoding="utf-8") as file:
+            sql_script = file.read()
+        cursor.execute(sql_script)
+    print("Setup complete.\n")
 
 def menu():
     pass
