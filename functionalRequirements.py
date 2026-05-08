5460fcb63165e5f2fc11ea06d5a9743382a4a9b5
jh7gz <jh7gz@umsystem.edu>
2026-05-08 14:37:50 -0500

    Emails can now be created when demo is not loaded

--- functionalRequirements.py
+++ functionalRequirements.py
@@ -54,13 +54,15 @@ def login():
                     quit()
 
                 try:
-                    mycursor.execute("SELECT MAX(User_ID) FROM USERS")
-                    thisID = mycursor.fetchone()[0] + 1
-                    mycursor.execute("INSERT INTO USERS(User_ID,Profile_Name,Password,F_name,M_Init,L_Name) VALUES (%s,%s,%s,%s,%s,%s)", (thisID,id,psswrd,first,middle,last))
+                    #mycursor.execute("SELECT MAX(User_ID) FROM USERS")
+                    #thisID = mycursor.fetchone()[0] + 1
+                    mycursor.execute("INSERT INTO USERS(Profile_Name,Password,F_name,M_Init,L_Name) VALUES (%s,%s,%s,%s,%s)", (id,psswrd,first,middle,last))
                     db.commit()
                 except mysql.connector.IntegrityError as err:
                     print("Error: {}".format(err))
                 try:
+                    mycursor.execute("SELECT User_ID FROM USERS WHERE Profile_Name = (%s)", (id,))
+                    thisID = mycursor.fetchone()[0]
                     mycursor.execute("INSERT INTO USER_EMAIL(User_ID,email) VALUES (%s,%s)",(thisID,email))
                     db.commit()
                 except mysql.connector.IntegrityError as err:
@@ -348,7 +350,7 @@ def updateUser(id:int):
                 alter = input("Which email would you like to add?")
                 try:
                     mycursor.execute("INSERT INTO USER_EMAIL(User_ID,email) VALUES (%s,%s)",(id,alter))
-                    db.commit
+                    db.commit()
                 except mysql.connector.IntegrityError as err:
                     print("Error: {}".format(err))
                 print("Email added successfully")
